eb966511f1d28873e368e82c376b5d5a825128ba
אלעד <elad12390@gmail.com>
2021-11-03 01:32:07 +0200

    css changes, and added functionality

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// import en from './i18n/en.js'
-// import he from './i18n/he.js'
-// import ar from './i18n/ar.js'
-
 const colorStrings = (obj, color) => {
     if (typeof obj !== 'object') {
         return `<span style=color:${color}>` + obj + '</span>';
@@ -48,33 +44,94 @@ const readFile = (evt, callback) => {
     reader.readAsText(file)
 }
 
-const setDiffDiv = (outputElement, firstFile, secondFile) => {
+const setDiffDiv = (outputElement, outputHeader, firstFile, firstFileName, secondFile, secondFileName) => {
     if (firstFile === null || secondFile === null) {
         return;
     }
+    outputHeader.innerHTML = `from language <b>${firstFileName.split('.')[0]}</b> to language <b>${secondFileName.split('.')[0]}</b>`;
     outputElement.innerHTML = JSON.stringify(diff(firstFile, secondFile), null, 2);
 }
 
-const loadPage = () => {
-    let firstFile = null, secondFile = null;
-    let firstFileName = null, secondFileName = null;
+class DebouncedListener {
+    timer;
+    callback;
+    delay;
+    constructor(delay, callback) {
+        this.callback = callback;
+        this.delay = delay;
+    }
+    invoke(ev) {
+        if (!!this.timer) {
+            clearTimeout(this.timer);
+        }
 
+        this.timer = setTimeout(() => {
+            if (!!this.callback && (typeof this.callback === 'function')) {
+                this.callback(ev);
+            }
+        }, this.delay)
+    }
+}
+
+const addDebouncedEventListener = (element, events, delay, callback) => {
+    if (!!element && !!events && events.length) {
+        for (const event of events) {
+            const listener = new DebouncedListener(delay, callback);
+            element.addEventListener(event, (ev) => listener.invoke(ev));
+        }
+    }
+}
+
+const loadPage = () => {
+
+    let firstFile = null, secondFile = null;
     const firstFileElement = document.getElementById('firstFile');
+    const firstFileButton = document.getElementById('firstFileButton');
+
+    let firstFileName = null, secondFileName = null;
     const secondFileElement = document.getElementById('secondFile');
+    const secondFileButton = document.getElementById('secondFileButton');
+
+    const outputWrapper = document.getElementById('outputWrapper');
+    const outputHeader = document.getElementById('outputHeader');
     const outputElement = document.getElementById('output');
     const downloadElement = document.getElementById('download');
     const downloadLink = document.createElement('a');
-    
+
+    // outputWrapper.hidden = true;
+
+    firstFileButton.addEventListener('click', () => {
+        firstFileElement.click();
+    })
+
+    secondFileButton.addEventListener('click', () => {
+        secondFileElement.click();
+    })
+
+    addDebouncedEventListener(outputElement, ['keydown'], 400, () => {
+        const selectedText = window.getSelection();
+        const style = selectedText.anchorNode.parentElement.style;
+        if (style.color === 'red') {
+            style.color = 'green';
+        }
+    })
+
     firstFileElement.addEventListener('change', (ev) => readFile(ev, (json, fileName) => {
         firstFile = JSON.parse(json);
         firstFileName = fileName;
-        setDiffDiv(outputElement, firstFile, secondFile);
+        setDiffDiv(outputElement, outputHeader, firstFile, firstFileName, secondFile, secondFileName);
+        if (firstFile && secondFile) {
+            outputWrapper.hidden = false;
+        }
     }), false);
 
     secondFileElement.addEventListener('change', (ev) => readFile(ev, (json, fileName) => {
         secondFile = JSON.parse(json);
         secondFileName = fileName;
-        setDiffDiv(outputElement, firstFile, secondFile);
+        setDiffDiv(outputElement, outputHeader, firstFile, firstFileName, secondFile, secondFileName);
+        if (firstFile && secondFile) {
+            outputWrapper.hidden = false;
+        }
     }), false);
 
     downloadElement.addEventListener('click', async() => {
@@ -84,6 +141,7 @@ const loadPage = () => {
         downloadLink.download = fileName;
         downloadLink.click();
     })
+
     document.getElementById('copy').addEventListener('click', async () => {
         await navigator.clipboard.writeText(outputElement.innerText);
         alert('copied !');
